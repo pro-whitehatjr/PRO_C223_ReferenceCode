6773317c8d67202dfc3b767845688e4e0cc26483
VS VIJAY /வா.ச.விஜய்/वा.स.विजय/వా.సా.విజయ్ <85885759+VSVijay142010@users.noreply.github.com>
2023-09-05 18:05:01 +0530

    Update bruteforce_zipfolder_Reference_code.py

--- bruteforce_zipfolder_Reference_code.py
+++ bruteforce_zipfolder_Reference_code.py
@@ -1,19 +1,19 @@
-import zipfile                     #Library used for password encrypted zipped folder/file
+import zipfile                   
 import time
 
-folderpath = input('Path to the file: ')  #Get the target file path and name from the user
+folderpath = input('Path to the file: ')  
 folderpath = folderpath.strip()
-zipf = zipfile.ZipFile(folderpath)      #Initialize a PdfFileReader object                             
+zipf = zipfile.ZipFile(folderpath)
 
-if not zipf:           #Checks if the file is password encrypted
+if not zipf:          
     print('The zipped file/folder is not password protected! You can successfully open it!')  #Notifies if the zipped file/folder is not password encrypted
 
 else:
-    starttime = time.time()             #Save the start time
-    result = 0                          #Intialize a variable result with zero. '0' will indicate Failure, while '1' will idicate Success
-    c = 0                               #Initialize a variable c to keep the count of passwords tried
+    starttime = time.time()             
+    result = 0                         
+    c = 0                               
 
-    #Build a character array including all numbers,lowercase letter, uppercase letters and special haracters. Total 10+26+26+33 = 95 characters
+  
     characters =['0','1','2','3','4','5','6','7','8','9',
                  'a','b','c','d','e','f','g','h','i','j','l','k','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
                  'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','p','Q','R','S','T','U','V','W','X','Y','Z',
@@ -22,7 +22,7 @@ else:
     
     print("Brute Force Started...")
     
-    #If still the password is not found i.e. result = 0, the below loop will try four character passwords. 81450625 Possible Combinations        
+     
     if(result == 0):
         print("Checking for 4 character password...")
         for i in characters:
@@ -37,15 +37,15 @@ else:
                             with zipfile.ZipFile(folderpath,'r') as zf:
                                 zf.extractall(pwd=password)
                                 print("Success! The password is: "+ guess)
-                                endtime = time.time()            #Save the end time
-                                result = 1                       #Set result variable to 1 on success
-                                break                            #If the password is found break from i for loop
+                                endtime = time.time()           
+                                result = 1                       
+                                break                           
                         except:
                             pass
                     if result == 1:
-                        break                           #If the password is found break from j for loop
+                        break                          
                 if result == 1:
-                    break                               #If the password is found break from k for loop
+                    break                              
             if result == 1:
                 break                                   #If the password is found break from l for loop
 
